6f87b39945c6878af7c457f8c56587306224e032
seungyup <lsy9312242@naver.com>
2021-10-24 14:48:03 +0900

    클라이언트 -> 서버 호출 (Request + Client) 부분까지만 작성

--- app/src/main/java/com/example/colosseum_20211024/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_20211024/utils/ServerUtil.kt
@@ -1,6 +1,7 @@
 package com.example.colosseum_20211024.utils
 
 import okhttp3.FormBody
+import okhttp3.OkHttpClient
 import okhttp3.Request
 
 class ServerUtil {
@@ -30,8 +31,34 @@ class ServerUtil {
                 .post(formData)
                 .build()
 
+            // 만들어진 request를 실제로 호출 해야함.
+            // 서버에 요청을 실제로 하자. -> 클라이언트의 역할. -> 앱이 클라이언트로써 동작하게 하자.
+            val client = OkHttpClient()
+
+            client.newCall(request)
+
+
         }
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
