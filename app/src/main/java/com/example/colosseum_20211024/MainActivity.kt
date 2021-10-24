73386bf40cafc54e04fe3fbbf32a2bcfa617a2b2
seungyup <lsy9312242@naver.com>
2021-10-24 14:59:51 +0900

    서버응답중 본문 (respoinse -> body)를 받아서 로그로 출력   - 로그인 버튼 눌리면 로그인 시도 연결   - Callback으로 응답이 왔을때 처리 코딩중   - 한글이 깨져서 메세지를 못알아봄

--- app/src/main/java/com/example/colosseum_20211024/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_20211024/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.colosseum_20211024.databinding.ActivityMainBinding
+import com.example.colosseum_20211024.utils.ServerUtil
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +24,9 @@ class MainActivity : AppCompatActivity() {
             // 가져온 이메일 / 비번을 로그로 출력
             Log.d("입력이메일", inputEmail)
             Log.d("입력비번", inputPw)
+
+            // 서버의 로그인 기능에 전달.
+            ServerUtil.postRequestLogin(inputEmail, inputPw)
 
         }
     }
