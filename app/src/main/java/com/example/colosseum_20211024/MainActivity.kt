18c7d9c189c8eff746e0165833e959e9afdf3faa
seungyup <lsy9312242@naver.com>
2021-11-06 12:52:09 +0900

    회원가입화면 UI 작업

--- app/src/main/java/com/example/colosseum_20211024/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_20211024/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.colosseum_20211024
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -21,6 +22,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.signUpBtn.setOnClickListener {
+
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
         binding.loginBtn.setOnClickListener {
 
             // 입력한 이메일 / 비번을 데이터바인딩으로 가져오기.
