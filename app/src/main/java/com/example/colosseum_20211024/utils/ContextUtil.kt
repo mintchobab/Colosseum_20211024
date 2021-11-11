391c2e2ee84958520a52a4bfd8a5389dd57f078b
seungyup <lsy9312242@naver.com>
2021-11-12 06:52:00 +0900

    자동로그인 여부 저장/조회 기능 추가

--- app/src/main/java/com/example/colosseum_20211024/utils/ContextUtil.kt
+++ app/src/main/java/com/example/colosseum_20211024/utils/ContextUtil.kt
@@ -10,6 +10,24 @@ class ContextUtil {
 
         private val TOKEN = "TOKEN"
 
+        // 자동로그인 체크여부 저장
+        private val AUTO_LOGIN = "AUTO_LOGIN"
+
+        // 자동로그인 - setter / getter 작성
+
+        fun setAutoLogin(context: Context, isAuto: Boolean) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, isAuto).apply()
+
+        }
+
+        fun getAutoLogin(context: Context): Boolean {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(AUTO_LOGIN, false)
+        }
+
+
         // setter - 토큰 저장 기능. SAVE
         fun setToken(context: Context, token: String) {
 
@@ -21,8 +39,7 @@ class ContextUtil {
         }
 
         // getter - 토큰 조회 기능. LOAD
-
-        fun getToken(context: Context) : String {
+        fun getToken(context: Context): String {
             // 메모장을 불러내자.
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             // 불러낸 메모장에서 token 값 찾아서 리넡. (결과로 지정)
